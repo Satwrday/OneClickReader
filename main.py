dcc3e11ae8e6141bb8dc0c9f59add60b05a7477f
satwrday <mab.fanatique@gmail.com>
2026-05-05 00:05:06 +0200

    corrected text to be continuous

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class Reader:
     def __init__(self):
        
         self.process = None
+        self.speed = 45
+        self.speed_change_var = 6
 
     def _read_task(self, text,speed):
         # Engine must be initialized inside the new process
@@ -22,25 +24,47 @@ class Reader:
         self.stop_reading()
 
         # Switch to previous window, copy, and switch back
-        pyautogui.hotkey('alt', 'tab')
-        time.sleep(0.1)  # Give the OS a moment to switch focus
-        pyautogui.hotkey('ctrl', 'c')
-        time.sleep(0.1)
-        pyautogui.hotkey('alt', 'tab')
+        self.Go_to_prev_window()
+        time.sleep(0.01)  # Give the OS a moment to switch focus
+        self.Copy()
+        time.sleep(0.01)
+        self.Go_to_prev_window()
+        
 
-        text = str(pyperclip.paste())
+        text_paceholder = str(pyperclip.paste())
+        text = " ".join(text_paceholder.splitlines())
         print(f"Reading: {text}")
 
         if text.strip():
-            # Pass the function and arguments separately
-            self.process = m.Process(target=self._read_task, args=(text, 28))
+            # Pass the function and arguments separately              change speed here;;;;;;;;;;;;;
+            self.process = m.Process(target=self._read_task, args=(text, self.speed))
             self.process.start()
+            
+    def Copy(self):
+            pyautogui.keyDown('ctrl')
+            pyautogui.hotkey( 'c')
+            pyautogui.keyUp('ctrl')
+            
+    def Go_to_prev_window(self):
+        pyautogui.keyDown('alt')
+        pyautogui.hotkey( 'tab')
+        pyautogui.keyUp('alt')
 
     def stop_reading(self):
         if self.process and self.process.is_alive():
             self.process.terminate()
             self.process.join()
             print("Reading stopped.")
+            
+            
+    def inc_speed(self):
+        self.speed +=self.speed_change_var
+        self.main_reader_operation()
+    
+    def dec_speed(self):
+        self.speed -= self.speed_change_var
+        self.main_reader_operation()
+        
 
 def main():
     # Multiprocessing on Windows requires this inside the entry point
@@ -50,15 +74,21 @@ def main():
     
     root = tk.Tk()
     root.title("Reader v0.0.2")
-    root.geometry("300x200")
+    root.geometry("60x90")
     root.attributes("-topmost", True) # Keep it on top for easier use
 
-    tk.Label(root, text="Simple TTS Reader", pady=10).pack()
+    tk.Label(root, text="Reader", pady=10).pack()
     
     tk.Button(root, text="Read ", 
               command=reader_instance.main_reader_operation, 
               width=20, height= 3, bg="green", fg="white").pack(pady=5)
     
+    tk.Button(root, text="+speed ", 
+              command=reader_instance.inc_speed, 
+              width=10, height= 2, bg="green", fg="white").pack(pady=5)
+    tk.Button(root, text="-speed ", 
+              command=reader_instance.dec_speed, 
+              width=10, height= 2, bg="green", fg="white").pack(pady=5)
     tk.Button(root, text="Stop ", 
               command=reader_instance.stop_reading, 
               width=20, height= 3, bg="red", fg="white").pack(pady=5)
